ec55a784c4f559df11ec0831660edcf28613855b
JGoo99 <zzgg3040@naver.com>
2024-04-11 15:54:37 +0900

    bfs/dfs

---
+++ bfs/queue/qMaze.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#define MAX_SIZE 100
+
+int maze[MAX_SIZE][MAX_SIZE];
+int q[MAX_SIZE * MAX_SIZE];
+bool visit[MAX_SIZE][MAX_SIZE];
+
+int m, n;
+int head, tail;
+
+int di[] = {0, 0, -1, 1};
+int dj[] = {-1, 1, 0, 0};
+
+void validateSize(int& m, int& n) {
+  while (m > MAX_SIZE || n > MAX_SIZE) {
+    printf("미로의 최대 사이즈(100 * 100) 를 초과했습니다. 다시 입력해주세요: ");
+    cin >> m >> n;
+  }
+}
+
+void initMaze() {
+  printf("미로의 행(세로)과 열(가로)을 입력해주세요: ");
+  cin >> m >> n;
+
+  validateSize(m, n);
+
+  for (int i = 0; i < m; i++) {
+    printf("%d번 줄 (%d/%d) [0: 길, 1: 벽]: ", i + 1, i + 1, m);
+    for (int j = 0; j < n; j++) {
+      cin >> maze[i][j];
+    }
+  }
+  maze[0][0] = 0; // 시작 지점
+  maze[m - 1][n - 1] = 2; // 도착 지점
+}
+
+void initQ() {
+  head = tail = 0;
+}
+
+bool isEmpty() {
+  return head == tail;
+}
+
+void add(int x) {
+  q[tail++] = x;
+}
+
+int pop() {
+  return q[head++];
+}
+
+void printMaze() {
+  printf("<<<<< 미로 >>>>>\n");
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      printf("%d ", maze[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void printVisit() {
+  printf("<<< 지나간 길 >>>\n");
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      printf("%d ", visit[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+
+bool bfs() {
+  initQ();
+
+  add(0);
+  add(0);
+
+  while (!isEmpty()) {
+    int curI = pop();
+    int curJ = pop();
+
+    if (visit[curI][curJ]) {
+      continue;
+    }
+
+    if (maze[curI][curJ] == 2) {
+      visit[curI][curJ] = true;
+      return true;
+    }
+
+    visit[curI][curJ] = true;
+
+    for (int l = 0; l < 4; l++) {
+      int nextI = curI + di[l];
+      int nextJ = curJ + dj[l];
+
+      if (nextI < 0 || nextI > m - 1 || nextJ < 0 || nextJ > n - 1) continue;
+
+      if (maze[nextI][nextJ] != 1 && !visit[nextI][nextJ]) {
+        add(nextI);
+        add(nextJ);
+      }
+    }
+  }
+  return false;
+}
+
+
+int main() {
+  initMaze();
+
+  if (bfs()) {
+    cout << "출구를 찾았습니다!" << endl;
+  } else {
+    cout << "출구를 찾을 수 없습니다." << endl;
+  }
+
+  printMaze();
+  printVisit();
+}
