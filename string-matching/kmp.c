898d4e3f29e736cafdb1d84cff52af8b7e93483c
JGoo99 <zzgg3040@naver.com>
2024-04-28 20:51:26 +0900

    kmp

--- string-matching/kmp.c
+++ string-matching/kmp.c
@@ -25,12 +25,32 @@ int* getTable(char *pattern, int m) {
     return table;
 }
 
+void kmp(char *text, int n, char *pattern, int m, int *table) {
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        while (j > 0 && pattern[j] != pattern[i]) {
+            j = table[j - 1];
+        }
+        if (pattern[i] == pattern[j]) {
+            if (j == m) {
+                printf("%d번째 인덱스에서 찾았습니다.", i);
+            } else {
+                j++;
+            }
+        }
+    }
+}
+
 int main() {
+    char text[] = "abaaacabcabadadaabcabada";
+    int n = strlen(text);
     char pattern[] = "abcabada";
     int m = strlen(pattern);
     int *table = getTable(pattern, m);
 
     print(table, m);
+
+    kmp(text, n, pattern, m, table);
     free(table);
     return 0;
 }
